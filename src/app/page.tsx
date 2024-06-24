0d88d93eed8a06b2d153a9ae6c175f64f92d397b
Karamjeet  Singh <karamjeet@Karamjeets-MacBook-Air.local>
2024-06-24 11:38:01 -0700

    home page completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,111 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Link from "next/link";
+import { ArrowRight } from "lucide-react";
+import { buttonVariants } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
-      <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
-        <p className="text-sm font-semibold text-gray-700">
-          PDF-Chat is now public
+    <>
+      <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
+        <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
+          <p className="text-sm font-semibold text-gray-700">
+            PDF-Chat is now public
+          </p>
+        </div>
+        <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl leading-relaxed">
+          Chat with your{" "}
+          <span className="bg-gradient-to-r from-cyan-500 to-blue-500 text-white">
+            documents
+          </span>{" "}
+          in seconds.
+        </h1>
+        <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
+          PDF-Chat enables you to have conversation with any PDF document.
+          Simply upload your file and start asking questions right away.
         </p>
-      </div>
-    </MaxWidthWrapper>
+        <Link
+          className={buttonVariants({ size: "lg", className: "mt-5" })}
+          href="/dashboard"
+          target="_blank"
+        >
+          Get Started <ArrowRight className="ml-2 h-5 w-5" />
+        </Link>
+      </MaxWidthWrapper>
+      <MaxWidthWrapper className="my-10 md:my-20 rounded-lg">
+        <Image
+          src="/dashboard-preview.jpg"
+          alt=""
+          width={0}
+          height={0}
+          quality={100}
+          sizes="100vw"
+          className="w-full drop-shadow-lg"
+        />
+      </MaxWidthWrapper>
+
+      <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col justify-center text-start">
+        <h1 className="max-w-4xl text-3xl font-semibold md:text-4xl lg:text-5xl leading-relaxed">
+          Start chatting in minutes
+        </h1>
+        <p className="text-gray-600 mt-1">
+          Chatting with your PDF files has never been this easier.
+        </p>
+        <ol className="my-8 space-y-4 pt-8 md:flex md:space-x-12 md:space-y-0">
+          <li className="md:flex-1">
+            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4">
+              <span>Step 1</span>
+              <span className="text-xl font-semibold">
+                Sign up for an account
+              </span>
+              <span className="mt-2 text-zinc-700">
+                Either starting out a free plan or choose our{" "}
+                <Link className="text-blue-700 hover:underline" href="/pricing">
+                  pro plan.
+                </Link>
+              </span>
+            </div>
+          </li>
+
+          <li className="md:flex-1">
+            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4">
+              <span>Step 2</span>
+              <span className="text-xl font-semibold">
+                Upload your PDF file
+              </span>
+              <span className="mt-2 text-zinc-700">
+                We&apos;ll process your file and make it ready for you to chat
+                with.
+              </span>
+            </div>
+          </li>
+
+          <li className="md:flex-1">
+            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4">
+              <span>Step 3</span>
+              <span className="text-xl font-semibold">
+                Start asking questions
+              </span>
+              <span className="mt-2 text-zinc-700">
+                It&apos;s that simple. Try it yourself - it really takes less
+                than a minute.
+              </span>
+            </div>
+          </li>
+        </ol>
+      </MaxWidthWrapper>
+
+      <MaxWidthWrapper className="mb-20">
+        <Image
+          src="/file-upload-preview.jpg"
+          alt=""
+          width={0}
+          height={0}
+          quality={100}
+          sizes="100vw"
+          className="w-full drop-shadow-lg"
+        />
+      </MaxWidthWrapper>
+    </>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,14 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
-const config: Config = {
+const config = {
   darkMode: ["class"],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
+  prefix: "",
   theme: {
     container: {
       center: true,
@@ -16,9 +18,6 @@ const config: Config = {
       },
     },
     extend: {
-      maxWidth: {
-        "8xl": "1408px",
-      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -56,25 +55,26 @@ const config: Config = {
       },
       borderRadius: {
         lg: "var(--radius)",
-        md: "cal(var(--radius) - 2px)",
-        sm: "cal(var(--radius)-4px)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
-        "accordian-down": {
+        "accordion-down": {
           from: { height: "0" },
-          to: { height: "var(--radix-accordian-content-height)" },
+          to: { height: "var(--radix-accordion-content-height)" },
         },
-        "accordian-up": {
-          from: { height: "var(--radix-accordian-content-height)" },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
       },
       animation: {
-        "accordian-up": "accordian-up 0.2 easy-out",
-        "accordian-down": "accordian-down 0.2 easy-out",
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
-};
-export default config;
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
+export default config
