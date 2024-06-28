eeb926aaf94eb858d53098d97d04cf78fcfe81f7
Karamjeet Singh <singhkaramjeetaman@gmail.com>
2024-06-28 10:31:05 -0700

    dashboard card UI updated

--- src/components/DashboardFileCard.tsx
+++ src/components/DashboardFileCard.tsx
@@ -1,26 +1,39 @@
 import { File } from "@/utils/interfaces";
 import Link from "next/link";
-import Image from "next/image";
-import { Trash2 } from "lucide-react";
+import { Trash2, Plus, MessageSquare } from "lucide-react";
+import { format } from "date-fns";
+import { Button } from "./ui/button";
 
 const DashboardFileCard = ({ file }: { file: File }) => {
   return (
-    <div className="border p-2 rounded-md shadow-lg w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
-      <Link href={file.url} target="_blank">
-        <div className="flex justify-between">
-          <h1>{file.name}</h1>
-          <Trash2 className="hover:text-red-500" />
+    <div className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow transition-all hover:shadow-lg">
+      <Link href={`/dashboard/${file.id}`} className="flex flex-col gap-2">
+        <div className="pt-6 px-6 flex w-full items-center justify-between space-x-6">
+          <div className="h-10 w-10 flex-shrink-0 rounded-full bg-gradient-to-r from-cyan-500 to-blue-500" />
+          <div className="flex-1 truncate">
+            <div className="flex items-center space-x-3">
+              <h3 className="truncate text-lg font-medium text-zinc-900">
+                {file.name}
+              </h3>
+            </div>
+          </div>
         </div>
-        <hr className="my-2" />
-        <Image
-          src="/thumbnail.png"
-          alt=""
-          width={0}
-          height={0}
-          sizes="100vw"
-          className="w-full opacity-50 rounded-md"
-        />
       </Link>
+
+      <div className="px-6 mt-4 grid grid-cols-3 place-items-center py-2 gap-6 text-xs text-zinc-500">
+        <div className="flex items-center gap-2">
+          <Plus className="h-4 w-4" />
+          {format(new Date(file.createdAt), "MMM yyyy")}
+        </div>
+
+        <div className="flex items-center gap-2">
+          <MessageSquare className="h-4 w-4" /> Mocked
+        </div>
+
+        <Button size="sm" className="w-full" variant="destructive">
+          <Trash2 className="h-4 w-4" />
+        </Button>
+      </div>
     </div>
   );
 };
--- src/components/DashboardFiles.tsx
+++ src/components/DashboardFiles.tsx
@@ -48,7 +48,7 @@ const DashboardFiles = () => {
           </div>
         </div>
       ) : (
-        <div className="flex flex-wrap justify-between gap-4 p-4 sm:gap-6 md:gap-8 lg:gap-10">
+        <div className="mt-8 grid grid-cols-1 gap-6 divide-y divide-zinc-200 md:grid-cols-2 lg:grid-cols-3">
           {files.map((file) => (
             <DashboardFileCard key={file.id} file={file} />
           ))}
