946046249cf3b6a7955ecbf7dadf1d481436f310
Karamjeet Singh <singhkaramjeetaman@gmail.com>
2024-07-15 12:34:44 -0700

    stripe conf added, pricing page created

---
+++ src/utils/stripe.ts
@@ -0,0 +1,28 @@
+export const plans = [
+  {
+    name: "Free plan",
+    slug: "free",
+    quota: 10,
+    pagesPerPDF: 4,
+    price: {
+      amount: 0,
+      priceIds: {
+        test: "",
+        production: "",
+      },
+    },
+  },
+  {
+    name: "Pro",
+    slug: "pro",
+    quota: 50,
+    pagesPerPDF: 25,
+    price: {
+      amount: 9,
+      priceIds: {
+        test: "price_1PcuDLEU7jQAqbzevmhluL6W",
+        production: "",
+      },
+    },
+  },
+];
